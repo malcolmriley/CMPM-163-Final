0f9c7bb31277913d6cbd91f46b760aec8d975094
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-06-14 02:02:10 -0700

    Add FireProjectile boilerplate

---
+++ Assets/Scripts/FireProjectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// MonoBehavior for controlling the UFO object - Launches a projectile at a point when invoked.
+/// 
+/// Written by Malcolm Riley
+/// CMPM 163, Spring 2019
+/// </summary>
+public class FireProjectile : MonoBehaviour {
+
+	// Public Fields
+	[Header("References")]
+	public GameObject projectilePrefab;
+	public Transform projectileOrigin;
+
+	[Header("Configuration")]
+	public float projectileSpeed;
+
+	public void Fire(Vector3 vector) {
+
+	}
+}
