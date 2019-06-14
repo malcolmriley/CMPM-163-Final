5231a889d1629ff842f0cb3ec8339900e8681d34
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-06-14 04:32:36 -0700

    Implement AutoDestroy helper script

---
+++ Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// MonoBehavior for automatically destroying the parent GameObject.
+/// 
+/// Written by Malcolm Riley
+/// CMPM 163, Spring 2019
+/// </summary>
+public class AutoDestroy : MonoBehaviour {
+
+	// Public Fields
+	public float destroyAfter = 5.0F;
+
+	// Internal Fields
+	private float _progress;
+
+	public void Update() {
+		_progress = Mathf.Clamp01(_progress + (Time.deltaTime / destroyAfter)) {
+			if (_progress >= 1.0F) {
+				Destroy(this);
+			}
+		}
+	}
+}
