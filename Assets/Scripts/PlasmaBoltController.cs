e9e86334e57040ec7a3d74f47ae63701aa5b0a35
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-06-14 04:03:26 -0700

    Add script for destroying projectile

---
+++ Assets/Scripts/PlasmaBoltController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// MonoBehavior for controlling the Plasma Bolt prefab.
+/// 
+/// Written by Malcolm Riley
+/// CMPM 163, Spring 2019
+/// </summary>
+public class PlasmaBoltController : MonoBehaviour {
+
+	[Header("Configuration")]
+	public float destroyDelay = 5.0F;
+
+	[Header("Internal Use Only")]
+	public List<GameObject> destroyImmediately;
+
+	public void InitiateDestruction() {
+		foreach (GameObject instance in destroyImmediately) {
+			Destroy(instance);
+		}
+		Invoke("Destroy", destroyDelay);
+	}
+
+	// Internal Methods
+	private void SelfDestruct() {
+		Destroy(this);
+	}
+}
