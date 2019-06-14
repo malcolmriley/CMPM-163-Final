ad913a17f103d53b207a10558797c1473b2561fe
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-06-14 01:13:04 -0700

    Reimplement dissolve script
    Need public method call, and don't need on-click activation.

---
+++ Assets/Scripts/ControlDissolve.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// MonoBehavior for controlling a dissolve shader attached to a MeshRenderer.
+/// 
+/// Written by Malcolm Riley
+/// CMPM 163, Spring 2019
+/// </summary>
+public class ControlDissolve : MonoBehaviour {
+
+	// Public Fields
+	[Header("References")]
+	public Material dissolveMaterial;
+	public string dissolveProperty;
+
+	[Header("Configuration")]
+	[Range(0.0F, 30.0F)]
+	public float effectSpeed;
+
+	// Internal Fields
+	private bool isDissolving = false;
+	private float _progress;
+
+	public void BeginDissolve() {
+		_progress = 0.0F;
+		isDissolving = true;
+	}
+
+	public void Update() {
+		if (isDissolving) {
+			_progress = Mathf.Clamp01(_progress + (Time.deltaTime / effectSpeed));
+			dissolveMaterial.SetFloat(dissolveProperty, _progress);
+		}
+	}
+}
