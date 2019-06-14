e70a7b371326cb69886785ba3d73184b905e5ab9
Malcolm Riley <malcolmriley@users.noreply.github.com>
2019-06-14 01:39:22 -0700

    Add UFOWeaponController boilerplate

---
+++ Assets/Scripts/UFOWeaponController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// MonoBehavior for controlling the UFO object - fires a plasma bolt when the mouse is clicked.
+/// 
+/// Written by Malcolm Riley
+/// CMPM 163, Spring 2019
+/// </summary>
+public class UFOWeaponController : MonoBehaviour {
+	public void Start() {
+		
+	}
+
+	public void Update() {
+		
+	}
+}
